dbbc4738690ad4a9e4c7ac042b5da12de5b42343
KalkiNara <163158995+KalkiNara@users.noreply.github.com>
2025-08-22 11:16:14 +0530

    Add files via upload

---
+++ Exp 2/DAAExp2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+double power(double x, int y) {
+
+    if (y == 0)
+        return 1;
+
+    double temp = power(x, y / 2);
+
+    if (y % 2 == 0)
+        return temp * temp;
+    else {
+
+        if (y > 0)
+            return x * temp * temp;
+        else
+
+            return (temp * temp) / x;
+    }
+}
+
+int main() {
+    double x;
+    int y;
+    
+    cout << "Enter base (x): ";
+    cin >> x;
+    cout << "Enter exponent (y): ";
+    cin >> y;
+
+    cout << x << "^" << y << " = " << power(x, y) << endl;
+
+    return 0;
+}
+
