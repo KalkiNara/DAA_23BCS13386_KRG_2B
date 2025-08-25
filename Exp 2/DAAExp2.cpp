c18e0781ecb99e0c585f0cfab9a41b8f266fed87
KalkiNara <163158995+KalkiNara@users.noreply.github.com>
2025-08-25 16:08:51 +0530

    Update DAAExp2.cpp

--- Exp 2/DAAExp2.cpp
+++ Exp 2/DAAExp2.cpp
@@ -1,36 +1,22 @@
-#include <iostream>
-using namespace std;
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long binForm = n;
+        
+        if(n < 0) {
+            x = 1 / x;
+            binForm = -binForm;
+        }
 
-double power(double x, int y) {
+        double ans = 1;
 
-    if (y == 0)
-        return 1;
-
-    double temp = power(x, y / 2);
-
-    if (y % 2 == 0)
-        return temp * temp;
-    else {
-
-        if (y > 0)
-            return x * temp * temp;
-        else
-
-            return (temp * temp) / x;
+        while(binForm > 0) {
+            if(binForm % 2 == 1) {
+                ans *= x;
+            }
+            x *= x;
+            binForm /= 2;
+        }
+        return ans;
     }
-}
-
-int main() {
-    double x;
-    int y;
-    
-    cout << "Enter base (x): ";
-    cin >> x;
-    cout << "Enter exponent (y): ";
-    cin >> y;
-
-    cout << x << "^" << y << " = " << power(x, y) << endl;
-
-    return 0;
-}
-
+};
